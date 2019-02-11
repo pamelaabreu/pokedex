d716cd1009ee3519177a0245eea8615a13295aa7
Pamela Abreu <pamelaabreu@pursuit.com>
2019-02-10 20:34:05 -0500

    setup of Profile function returning simple JSX

---
+++ src/components/profile.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+const Profile = (props) => {
+
+    return (
+        <>
+        <h1>Hello Profile Page!</h1>
+        </>
+    );
+}
+
+export {Profile};
