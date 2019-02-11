4a029459fa6d0e6c9fb91aad277036c85a9aa47c
Pamela Abreu <pamelaabreu@pursuit.com>
2019-02-10 19:02:46 -0500

    setup of pokedex

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <h1>Replace this line with Search bar Component</h1>
+        <h1>Replace this line with Homepage Component</h1>
+        <h1>Replace this line with Profile Component</h1>
+      </>
+    );
+  }
+}
+
+export default App;
