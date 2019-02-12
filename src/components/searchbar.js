ecf26d81feecb0539ed1e444197e1c9d71cd61f1
CodeXRook <lacekitfly@gmail.com>
2019-02-11 20:40:16 -0500

    updated searchbar

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -1,14 +1,35 @@
-import React ,{Component} from 'react';
+import React from 'react';
+import './searchbar.css';
 
-import{
-    Container,
-    Nav,
-    NavItem,
-    NavLink,
-}from 'reactstrap';
+ class Searchbar extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        initialState: "Search for pokemon",
+        currentText: " "
+      }
+    }
+  
+    changeText(currentText) {
+      this.setState({currentText});
+      //console.log({currentText});
+    }
+  
+    render() {
+      return (
+          <>
+           
+            <div class="search-box">
+              <form>
+                <input type="text" placeholder={this.state.initialState} onChange={this.changeText.bind(this, 'currentText')} />
+                <button onClick={this.changeText.bind(this, 'currentText')}>Search</button>
+              </form>
+            </div>
+          </>
+      );
+  
+    }
+  
+  }
 
-const Searchbar =(props)=>{
-    return (<h1>This is the searchbar lol</h1>)
-}
-
-export {Searchbar};
+  export default Searchbar;
