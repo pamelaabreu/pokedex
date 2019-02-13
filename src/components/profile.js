ddf0e9a14c97fcf31858bc4cb9f2f04b9368b594
Pamela Abreu <pamelaabreu@pursuit.com>
2019-02-13 01:11:02 -0500

    commented the baseStats for now

--- src/components/profile.js
+++ src/components/profile.js
@@ -4,12 +4,13 @@ import './profile.css';
 const Profile = props => {
 
     const {data} = props;
+    console.log('inside the profile',data);
     const {name, 
             id, 
             image, 
             sprites, 
             types, 
-            baseStats,
+            baseStatsArr,
             moves} = data;
 
     const spritesImg = sprites.map((e, i) => {
@@ -23,19 +24,18 @@ const Profile = props => {
     });
 
     // Basestat is an array of objects now, fix this
-    const bsArr = Object.entries(baseStats);
-    const bsCard = bsArr.map((v, i,) => {
-        let key= v[0];
-        let value = v[1];
-        return (
-            <>
-            <div className='bs-card'>
-                <h1 className='bs-info'>{key}</h1>
-                <h1 className='bs-num'>{value}</h1>
-            </div>
-            </>
-            );
-    });
+    // const bsCard = baseStatsArr.map((v, i,) => {
+    //     let key= v.statName;
+    //     let value = v.val;
+    //     return (
+    //         <>
+    //         <div className='bs-card'>
+    //             <h1 className='bs-info'>{key}</h1>
+    //             <h1 className='bs-num'>{value}</h1>
+    //         </div>
+    //         </>
+    //         );
+    // });
 
     const moveButton = moves.map((v, i) => {
         return <h1 key={i} className='m-button'>{v.name}</h1>
@@ -62,7 +62,7 @@ const Profile = props => {
             
             <h1 className='bs-header'>Base Stats</h1>
             <div className='bs-container'>
-            {bsCard}
+                {/* {bsCard} */}
             </div>
 
             <h1 className='bs-header'>Moves</h1>
