30812360098d11ea40d1e81e04737d50bd223834
Pamela Abreu <pamelaabreu@pursuit.com>
2019-02-14 20:01:49 -0500

    cchanged where the offset variable was

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Profile from './components/profile';
 import {Homepage} from './components/homepage';
 import Searchbar from './components/searchbar';
+let offset = 0;
 
 class App extends Component {
   constructor(props){
@@ -17,14 +18,13 @@ class App extends Component {
   }
 
   getlist = () => {
-    let offset = 0;
     let listUrl = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=20`;
 
     Axios.get(listUrl)
       .then(response => response.data)
       .then(data => {
         offset += 20;
-
+  
         if(this.state.poke_list.length >= 20){
           const dataList = data.results.map((e, i) => {
             const name = e.name;
@@ -67,8 +67,8 @@ class App extends Component {
   }
 
   componentDidUpdate (prevProps, prevState) {
-    // console.log('this was previous state', prevState)
-    // console.log('this is current state', this.state)
+    console.log('this was previous state', prevState)
+    console.log('this is current state', this.state)
   }
 
   render() {
