595fa51c2c19845010be6100150ea368e07b0076
Pamela Abreu <pamelaabreu@pursuit.com>
2019-02-19 14:03:57 -0500

    added modal component that takes in props

---
+++ src/components/modal.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import './modal.css';
+
+class Modal extends Component {
+    state = { show: false };
+  
+    showModal = (e) => {
+    // this.props.click(v.name)
+      this.setState({ show: true });
+    }
+    
+    hideModal = () => {
+      this.setState({ show: false });
+    }
+
+    moveButton = () => { 
+        const moveButton = this.props.data.moves.map((v, i) => {
+            return <h1 key={i} onClick={this.showModal} className='m-button'>{v.name}</h1>
+        });
+
+        return moveButton;
+    }
+      
+    render() {
+        const {type, power, pp} = this.props.data.moveInfo;
+            return (
+                <>
+                <Popup show={this.state.show} handleClose={this.hideModal} >
+                    <p className='m-name'>Cut</p>
+                    <p className='m-info' style={{textTransform: 'capitalize'}}>Type: {type}</p>
+                    <p className='m-info'>Power: {power}</p>
+                    <p className='m-info'>PP: {pp}</p>
+                </Popup>
+                {this.moveButton()}
+                </>
+            );
+    }
+  }
+  
+  const Popup = ({ handleClose, show, children }) => {
+    const showHideClassName = show ? 'modal display-block' : 'modal display-none';
+  
+    return (
+      <div className={showHideClassName}>
+        <section className='modal-main'>
+          <button className='close-button' onClick={handleClose}>X</button>
+          {children}
+        </section>
+      </div>
+    );
+  };
+
+  export default Modal;
